8a924252b2ce679d3f26c28d7cc20816860bd022
Pedro Trevino <trevinop36@gmail.com>
2025-09-23 19:53:05 +0200

    prism

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,15 +1,23 @@
 import Link from 'next/link';
-import Balatro from './Balatro';
+import Prism from './Prism';
 
 export function Footer() {
     return (
         <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
             <p className="text-sm">
-                <Balatro
-                    isRotate={false}
-                    mouseInteraction={true}
-                    pixelFilter={700}
+            <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+                <Prism
+                    animationType="rotate"
+                    timeScale={0.5}
+                    height={3.5}
+                    baseWidth={5.5}
+                    scale={3.6}
+                    hueShift={0}
+                    colorFrequency={1}
+                    noise={0.5}
+                    glow={1}
                 />
+            </div>
                 <Link
                     href="https://kinsta.com/es/base-de-conocimiento/next-js/"
                     className="decoration-dashed text-primary underline-offset-8"
