78bae99c2dc0326c6b2ec387cfd9d6a158c8a842
Pedro Trevino <trevinop36@gmail.com>
2025-09-23 19:47:59 +0200

    new elements meerge

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,15 @@
 import Link from 'next/link';
+import Balatro from './Balatro';
 
 export function Footer() {
     return (
         <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
             <p className="text-sm">
+                <Balatro
+                    isRotate={false}
+                    mouseInteraction={true}
+                    pixelFilter={700}
+                />
                 <Link
                     href="https://kinsta.com/es/base-de-conocimiento/next-js/"
                     className="decoration-dashed text-primary underline-offset-8"
