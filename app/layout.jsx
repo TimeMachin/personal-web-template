5db9711c1d9d879e4755c94425b4cb2519439268
Pedro Trevino <trevinop36@gmail.com>
2025-09-28 15:16:15 +0200

    final touch to whatsapp

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,8 +21,8 @@ export default function RootLayout({ children }) {
                         <Header />
                         <main className="grow">{children}</main>
                         <Footer />
-                        <a href="https://wa.me/34687093223/?text=tu%20texto%20personalizado" target="_blank">
-                            <img src="https://ebweb.es/wp-content/uploads/2020/09/logo-wasap.png" width="50" height="50"/>
+                        <a href="https://wa.me/34687093223" target="_blank">
+                            <img src="https://ebweb.es/wp-content/uploads/2020/09/logo-wasap.png" width="50" height="50" alt="Whatsapp logo"/>
                         </a>
                     </div>
                 </div>
