afbabe113a9359355286d80a56f528d43478b509
Pedro Trevino <trevinop36@gmail.com>
2025-09-28 15:42:48 +0200

    back

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
-import LetterGlitch from '../components/LetterGlitch';
+import { LetterGlitch } from '../components/LetterGlitch';
 
 export const metadata = {
     title: {
@@ -16,19 +16,19 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body>
-                <div>
+            <body className="antialiased text-white bg-blue-900">
+                <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
                     <LetterGlitch
-                        glitchSpeed={50}
-                        centerVignette={true}
-                        outerVignette={false}
-                        smooth={true}
-                    />
-                    <div>
+                    glitchSpeed={50}
+                    centerVignette={true}
+                    outerVignette={false}
+                    smooth={true}
+                />
+                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
                         <main className="grow">{children}</main>
                         <Footer />
-                        <a href="https://wa.me/34687093223" target="_blank">
+                        <a href="https://wa.me/34687093223/" target="_blank">
                             <img src="https://ebweb.es/wp-content/uploads/2020/09/logo-wasap.png" width="50" height="50" alt="Whatsapp logo"/>
                         </a>
                     </div>
