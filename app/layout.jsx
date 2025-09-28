a8c8eab946c0f598cc298868c44306ee3a5a8362
Pedro Trevino <trevinop36@gmail.com>
2025-09-28 20:05:17 +0200

    local development changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
 import LetterGlitch from '../components/LetterGlitch';
+import { fixedSize } from './blobs/generator';
 
 export const metadata = {
     title: {
@@ -16,23 +17,76 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
+            <body >
+                <div 
+                    style={{
+                        position: "fixed",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                        zIndex: -1,         // Se queda atrás
+                        overflow: "hidden", // Evita scroll raro
+                        opacity: 1
+                    }}
+                >
                     <LetterGlitch
-                        glitchSpeed={50}
+                        glitchSpeed={10}
                         centerVignette={true}
-                        outerVignette={false}
+                        outerVignette={true}
                         smooth={true}
                     />
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <Header />
-                        <main className="grow">{children}</main>
-                        <Footer />
-                        <a href="https://wa.me/34687093223/" target="_blank">
-                            <img src="https://ebweb.es/wp-content/uploads/2020/09/logo-wasap.png" width="50" height="50" alt="Whatsapp logo"/>
-                        </a>
-                    </div>
                 </div>
+
+
+
+
+
+
+
+{/*                 <div className="flex flex-col w-full max-w-5xl mx-auto grow">
+                    <Header />
+                    <main className="grow">{children}</main>
+                    <Footer />
+                </div> */}
+
+
+
+
+
+                    <nav className="sticky top-4 z-50">
+                        <div className="max-w-6xl mx-auto px-4">
+                            <div className="flex items-center justify-between 
+                                            bg-white/10 backdrop-blur-md 
+                                            rounded-4xl shadow-lg 
+                                            px-6 py-3
+                                            border border-white/50">
+                                <div className="text-2xl font-bold text-white">Pedro Rodriguez Treviño</div>
+                                <div className="hidden md:flex space-x-8">
+                                    <a href="#about" className="nav-link py-2 px-3 text-white hover:text-gray-300">About</a>
+                                    <a href="#experience" className="nav-link py-2 px-3 text-white hover:text-gray-300">Experience</a>
+                                    <a href="#education" className="nav-link py-2 px-3 text-white hover:text-gray-300">Education</a>
+                                    <a href="#skills" className="nav-link py-2 px-3 text-white hover:text-gray-300">Skills</a>
+                                    <a href="#projects" className="nav-link py-2 px-3 text-white hover:text-gray-300">Projects</a>
+                                    <a href="#contact" className="nav-link py-2 px-3 text-white hover:text-gray-300">Contact</a>
+                                </div>
+                                <button className="md:hidden">
+                                    <i className="fas fa-bars text-gray-800 text-2xl"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </nav>
+
+
+
+
+
+
+
+                    <a href="https://wa.me/34687093223/" target="_blank">
+                        <img src="https://ebweb.es/wp-content/uploads/2020/09/logo-wasap.png" width="50" height="50" alt="Whatsapp logo"/>
+                    </a>
+
             </body>
         </html>
     );
--- components/footer.jsx
+++ components/footer.jsx
@@ -6,26 +6,11 @@ export function Footer() {
     return (
         <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
             <p className="text-sm">
-
-                <div>
-{/*                     <Prism
-                        animationType="rotate"
-                        timeScale={0.5}
-                        height={3.5}
-                        baseWidth={5.5}
-                        scale={3.6}
-                        hueShift={0}
-                        colorFrequency={1}
-                        noise={0.5}
-                        glow={1}
-                    /> */}
-
-                </div>
                 <Link
                     href="https://kinsta.com/es/base-de-conocimiento/next-js/"
                     className="decoration-dashed text-primary underline-offset-8"
                 >
-                    Next.js on Netlify
+                    Next on Netlify
                 </Link>
             </p>
         </footer>
