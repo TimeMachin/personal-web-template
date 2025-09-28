e1f3d39b188052cdf3a4a01b33cd364b476e39d6
Pedro Trevino <trevinop36@gmail.com>
2025-09-28 15:37:48 +0200

    lg

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import LetterGlitch from './LetterGlitch';
 
 export const metadata = {
     title: {
