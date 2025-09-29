ad04eabd21a63222ad8f23764a82b9c2e5b264f2
Pedro Trevino <trevinop36@gmail.com>
2025-09-29 17:09:53 +0200

    images correction

--- components/contactsStack.jsx
+++ components/contactsStack.jsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 const ContactStack = ({}) => {
     return (
         <a href="https://wa.me/34687093223/" target="_blank">
-            <img src="https://ebweb.es/wp-content/uploads/2020/09/logo-wasap.png" width="50" height="50" alt="Whatsapp logo"/>
+            <Image
+                src="/images/whatsapp-logo.png"
+                width={50}
+                height={50}
+                alt="Contact"
+            />
         </a>
     );
 }
